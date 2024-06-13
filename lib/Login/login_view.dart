076ca5631305d4d5872cab6dcb9105fed1bc86ec
SAMAN <faireenazaidi@gmail.com>
2024-06-14 00:09:48 +0530

    signUp

--- lib/Login/login_view.dart
+++ lib/Login/login_view.dart
@@ -9,166 +9,174 @@ import '../Widgets/app_color.dart';
 import '../Widgets/myButton.dart';
 import '../Widgets/primary_text_field.dart';
 import 'login_controller.dart';
+
 class LoginView extends GetView<LoginController> {
   const LoginView({super.key});
+  
+  // Getter for _isChecked
+
+    @override
+    Widget build(BuildContext context) {
+      Color myColor = Colors.black.withOpacity(0.2);
+      // TODO: implement build
+
+      return SafeArea(
+          child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Container(
+                height: Get.height, width: Get.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/bg.png'),
+                      fit: BoxFit.fitHeight,
+                    )
+                ),
 
 
-  @override
-  Widget build(BuildContext context) {
-    Color myColor=Colors.black.withOpacity(0.2);
-    // TODO: implement build
-
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          height: Get.height,width: Get.width,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/bg.png'),
-                fit: BoxFit.fitHeight,
-              )
-          ),
-
-
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30.0,left: 8.0,right: 8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: HexColor('DDB887')),
-                      borderRadius: BorderRadius.circular (15),
-                    ),
-
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-
-                        child: Image.asset("assets/ddlogo.png",height: 135,)),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-
-                  Text("Login",style: MyTextTheme.veryLargeWCB,),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  PrimaryTextField(
-                    prefixIcon: Icon(Icons.phone_android_rounded),
-                      hintText: "Enter mobile number",
-                    backgroundColor: Colors.white,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-
-                  PrimaryTextField(
-                      prefixIcon: Icon(Icons.lock_outline_rounded),
-                      hintText: "Enter password",
-
-
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-
-                    children: [
-                      Checkbox(
-                        checkColor: Colors.green,
-                        activeColor: Colors.transparent,
-                        value: false, onChanged:(bool?value) {
-                        },
-                      ),
-                      Text("Remember me",style: MyTextTheme.mediumBCN,),
-                      SizedBox(
-                        width: 85,
-                      ),
-                      Text("Forgot Password?",style: MyTextTheme.mediumBCN)
-                    ],
-                  ),
-
-                  SizedBox(
-                    height: 20,
-                  ),
-
-                MyButton(
-                  borderRadius: 10,
-                  elevation: 2,
-                  width: 353,
-                  onPressed: (){}, title: "SIgn in",
-                color: AppColor.buttonColor,suffixIcon: Icon(Icons.arrow_forward,color: Colors.white,),),
-
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                      children: <Widget>[
-                        Expanded(
-                            child: Divider(
-                              color: Colors.white,
-                              indent: 80,
-                              endIndent: 1,
-                              thickness: 0.6,
-                            )
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-
-                        Text("or",style: MyTextTheme.mediumBCN,),
-
-                        Expanded(
-                            child: Divider(
-                              color: Colors.white,
-                              endIndent: 80,
-                              indent: 10,
-                              thickness: 0.6,
-                            )
-                        ),
-                      ]
-                  ),
-                  SizedBox(height: 10,),
-                  Text("Login with OTP",style: MyTextTheme.largeWCB,),
-                  SizedBox(
-                    height: 10,
-                  ),
-                RichText(
-                      text: TextSpan(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, left: 8.0, right: 8.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-              TextSpan(
-                text: "If you don't have an account registered,",
-                style:MyTextTheme.mediumBCN
-              ),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: HexColor('DDB887')),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
 
-              TextSpan(
-                text: "you can",style: MyTextTheme.mediumBCN
-              ),
-              TextSpan(
-                text: " Register here!",
-                style: MyTextTheme.largeBCB,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
 
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                  Get.toNamed(AppRoutes.signUpRoute);
+                                child: Image.asset(
+                                  "assets/ddlogo.png", height: 135,)),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
 
-                    // Navigate to registration screen or perform action
-                  },
-              ),
-                ],
-              ),
-                        ),
+                          Text("Login", style: MyTextTheme.veryLargeWCB,),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          PrimaryTextField(
+                            prefixIcon: Icon(Icons.phone_android_rounded),
+                            hintText: "Enter mobile number",
+                            backgroundColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+
+                          PrimaryTextField(
+                            prefixIcon: Icon(Icons.lock_outline_rounded),
+                            hintText: "Enter password",
+
+
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Checkbox(
+                                checkColor: Colors.green,
+                                activeColor: Colors.transparent,
+                                value: false, onChanged: (bool?value) {},
+                              ),
+                              Text(
+                                "Remember me", style: MyTextTheme.mediumBCN,),
+                              SizedBox(
+                                width: 85,
+                              ),
+                              Text("Forgot Password?",
+                                  style: MyTextTheme.mediumBCN)
+                            ],
+                          ),
+
+                          SizedBox(
+                            height: 20,
+                          ),
+
+                          MyButton(
+                            borderRadius: 10,
+                            elevation: 2,
+                            width: 353,
+                            onPressed: () {},
+                            title: "SIgn in",
+                            color: AppColor.buttonColor,
+                            suffixIcon: Icon(
+                              Icons.arrow_forward, color: Colors.white,),),
+
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                              children: <Widget>[
+                                Expanded(
+                                    child: Divider(
+                                      color: Colors.white,
+                                      indent: 80,
+                                      endIndent: 1,
+                                      thickness: 0.6,
+                                    )
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+
+                                Text("or", style: MyTextTheme.mediumBCN,),
+
+                                Expanded(
+                                    child: Divider(
+                                      color: Colors.white,
+                                      endIndent: 80,
+                                      indent: 10,
+                                      thickness: 0.6,
+                                    )
+                                ),
+                              ]
+                          ),
+                          SizedBox(height: 10,),
+                          Text("Login with OTP", style: MyTextTheme.largeWCB,),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                    text: "If you don't have an account registered,",
+                                    style: MyTextTheme.mediumBCN
+                                ),
+
+                                TextSpan(
+                                    text: "you can",
+                                    style: MyTextTheme.mediumBCN
+                                ),
+                                TextSpan(
+                                  text: " Register here!",
+                                  style: MyTextTheme.largeBCB,
+
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Get.toNamed(AppRoutes.signUpRoute);
+
+                                      // Navigate to registration screen or perform action
+                                    },
+                                ),
+                              ],
+                            ),
+                          ),
                         ]
-                      ),
-            ),
-          ),
-            )
-      )
-    );
+                    ),
+                  ),
+                ),
+              )
+          )
+      );
+    }
   }
-}
 
--- lib/signUp/signUp.dart
+++ lib/signUp/signUp.dart
@@ -3,17 +3,121 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import '../Widgets/text_theme.dart';
 
 class SignUpView extends GetView<SignUpController>{
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool isEmail=false;
+    String countryCode="+91";
+    String _selectedOption='mobile';
     // TODO: implement build
     return Scaffold(
-
+        resizeToAvoidBottomInset: false,
+        body: Container(
+        height: Get.height, width: Get.width,
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage('assets/bg.png'),
+    fit: BoxFit.fitHeight,
+    )
+    ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(  top: 30.0, left: 8.0, right: 8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+              Container(
+              decoration: BoxDecoration(
+              border: Border.all(color: HexColor('DDB887')),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+            
+                  child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+            
+                  child: Image.asset(
+                  "assets/ddlogo.png", height: 135,)),
+                  ),
+                  SizedBox(
+                  height: 30,
+                  ),
+                    Text("Login", style: MyTextTheme.veryLargeWCB,),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Radio<String>(value: 'mobile', groupValue: _selectedOption, onChanged: (value){
+                          setState(() {
+                            _selectedOption=value!;
+                          });
+                        }),
+                        Text("Mobile"),
+                        Radio<String>(value: 'Email', groupValue: _selectedOption, onChanged: (value){
+                          setState(() {
+                            _selectedOption=value!;
+                          });
+                        }),
+                        Text("Email"),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    if(!isEmail)
+                      Row(
+                        children: [
+                          DropdownButton<String>(
+                              value: countryCode,items: <String>['+91','+1','+44','+61'].map((String value) {
+                                return DropdownMenuItem<String>(
+                                    value:value,
+                                    child: Text(value),);
+                          }).toList(), onChanged: (newValue){
+                                setState(() {
+                                  countryCode=newValue!;
+                                });
+                          }),
+                          Expanded(child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Phone Number',
+                            ),
+                            keyboardType: TextInputType.phone,
+                          ))
+                        ],
+                      )
+                    else
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Enter email"
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(onPressed: (){}, child: Text('Send OTP')),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextButton(onPressed: (){}, child: Text('Already registered, you can login here!',style: MyTextTheme.mediumBCB,))
+                  ]
+                  ),
+            ),
+          )
+        )
     );
 
   }
+
+  void setState(Null Function() param0) {}
 
 }
